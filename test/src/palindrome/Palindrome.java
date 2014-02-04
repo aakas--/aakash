bcb54cf0150b693d37c06c6e59992a251f6978b8
aakas <aakashsahani@lftechnology.com>
2014-02-04 13:49:21 +0545

    lab-1 lab task completed

--- test/src/palindrome/Palindrome.java
+++ test/src/palindrome/Palindrome.java
@@ -5,7 +5,7 @@ package palindrome;
 
 import java.util.Scanner;
 
-/**
+/**This is a program that reads in a five-digit integer and determines whether it is a palindrome. 
  * @author aakas
  *
  */
@@ -15,44 +15,41 @@ public class Palindrome {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+				prompt();
+		
+	}
+	
+	public static void prompt(){
 		Integer num;
 		System.out.println("Please enter 5 digit number");
 		Scanner input= new Scanner(System.in);
 		num= input.nextInt();
 		
 		int len= String.valueOf(num).length();
-		
-	//System.out.println(reversDigits(num));
-			
 		if(len==5){
 			result(num);
 				
 		}
 		else{
 			System.out.println("Invalid Input");
-				
+			prompt();	
 		}
+		
 	}
-	
-	
 	private static void result(int number){
 		Integer reverseDigit= reversDigits(number);
 		if(number== reverseDigit)
 			System.out.println("The number is palindrome");
 		else
 			System.out.println("The number is not palindrome");
-			
-		
-		
-	}
+		}
+	
 	
 	public static int reversDigits(int number) {
 		int reversedNumber=0;
 		while(number > 0){
-            
             int temp = number%10;
-           
-            //create the reversed number
             reversedNumber = reversedNumber * 10 + temp;
             number = number/10;
              }
---
+++ test/src/tempconversion/TempConversion.java
@@ -0,0 +1,22 @@
+package tempconversion;
+
+public class TempConversion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+				
+		for(int cel=1;cel<=100;cel++)
+		{
+			System.out.println("Celcius ="+ cel +" "+ "Fahrenheit ="+ fahr(cel));
+		}
+				
+	}
+	
+	public static float fahr(float c)
+	{
+		return(c*9/5+32);
+	}
+
+}
